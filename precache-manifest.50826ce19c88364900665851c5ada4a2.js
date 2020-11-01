2a09a13fcc34db08cd729bd77477582d0ac19c65
zangzangs <kimjangh94@gmail.com>
2020-11-01 23:32:08 +0900

    Updates

---
+++ precache-manifest.50826ce19c88364900665851c5ada4a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6217327210270f781e1523f543612255",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "76263fd0c37e2ee3c75f",
+    "url": "/movie_app_2020/static/css/main.136bfb17.chunk.css"
+  },
+  {
+    "revision": "430e24c81aaf6231ccf6",
+    "url": "/movie_app_2020/static/js/2.b053e357.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.b053e357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76263fd0c37e2ee3c75f",
+    "url": "/movie_app_2020/static/js/main.f3ff988a.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
