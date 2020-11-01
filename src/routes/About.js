5ea81c817c1aab8e0ef8925f88153c60de13d5ff
zangzangs <kimjangh94@gmail.com>
2020-11-01 23:26:06 +0900

    최종 소스코드

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import './About.css';
 
-function About() {
-    return <span>About this page: I built it because I love movies.</span>;
+function About(props) {
+    //console.log(props);
+    return (
+    
+        <div className="about__contaner">
+            <span>
+                "Freedom is the freedom to say that two plus make four. If that is granted, all else follows."
+            </span>
+            <span> - George Orwell, 1984</span>
+
+            <div>
+            <a href="https://github.com/ZANGZANGS">github.com/ZANGZANGS</a>
+            </div>
+        </div>
+        
+        
+    );
 }
 
 export default About;
